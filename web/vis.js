250fe581b635b09cd1595416df5c1c56e8e6cddc
Daniel Zou <danielwzou@gmail.com>
2017-04-17 16:36:25 -0500

    updated viz

--- web/vis.js
+++ web/vis.js
@@ -47,11 +47,15 @@ var visualize = function(data) {
   majorNames = _.uniq(majorNames);
 
   var enrollmentIncPer = _.map(data, "TotalIncrPer");
+  var salary = _.map(data, "Salary75");
+  
 
   for(var i=0; i<enrollmentIncPer.length; i++) { enrollmentIncPer[i] = +enrollmentIncPer[i]; }
+  for(var i=0; i<salary.length; i++) { salary[i] = +salary[i]; }
   console.log(d3.min(enrollmentIncPer));
   var enrollmentScale = d3.scaleLinear()
-                    .domain( [d3.min(enrollmentIncPer), d3.max(enrollmentIncPer)] )
+                    .domain( [Math.log(d3.min(enrollmentIncPer)+1), 
+                              Math.log(d3.max(enrollmentIncPer)+1)] )
                     .range( [0, width] );
 
   var majorScale = d3.scaleBand()
@@ -62,11 +66,22 @@ var visualize = function(data) {
 
   var yAxis = d3.axisLeft().scale(majorScale);
 
-  svg.append("g").attr("transform", "translate(0, 340)").call(xAxis);
+  //svg.append("g").attr("transform", "translate(0, 340)").call(xAxis);
   svg.append("g").attr("transform", "translate(340, 0)").call(yAxis);
+  svg.append("text")
+            .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+            .attr("transform", "translate(" + (width / 2) + " ," + -20 + ")")
+            .text("---> Enrollment Growth --->");
+  svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("---> Salary --->");
 
   var tip = d3.tip().attr("class", "d3-tip").html(function(d){
-    return d['Major'];
+    return d['Major'] + ": " + 'Enrollment: ' + d['Total2015'] + ', 75th pct. Salary: ' + d['Salary75'];
   });
   svg.call(tip);
 
@@ -80,12 +95,17 @@ var visualize = function(data) {
         return 4;
       })
      .attr("cx",function (d) {
-       console.log(enrollmentScale(+d['TotalIncrPer']));
-      return enrollmentScale(+d['TotalIncrPer']);
+       //console.log(enrollmentScale(+d['TotalIncrPer']));
+      return enrollmentScale(Math.log(d['TotalIncrPer']+1));
      }) // center x pixel
      .attr("cy",function (d) {
       return majorScale(d['Major'])+10;
      }) // center x pixel
-     .attr("fill","purple")
+     .attr("fill", function (d) {
+      //  var str = d['Salary'] / d3.max(salary) * 255
+      //  console.log("color: " + str)
+      //  return "rgb(" + str + "," + "0" + ",0)"
+      return "purple"
+     })
      ;
 };
